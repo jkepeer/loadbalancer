7088921da1cf1934cc3331aebd27202ef6c29f86
root <jkepeer@gmail.com>
2018-05-24 05:09:59 +0300

    new

---
+++ script/start.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+cat ../conf/nginx1.conf > ../nginx.conf
+
+docker ps | grep user | awk '{print $13}'| cut -b 1-13 | sed 's/0.0.0.0/server 127.0.0.1/'| sed 's/$/;/' >> ../nginx.conf
+
+cat ../conf/nginx2.conf >> ../nginx.conf
+cd ..
+docker build -t jkepeer1/loadbalanser .
+docker run -d -p 80:80 jkepeer1/loadbalanser
