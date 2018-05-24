2aa90edc8afdf6a26023a61612d6f5b0121f0d97
Anton <jkepeer@gmail.com>
2018-05-24 13:02:10 +0300

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -9,7 +9,7 @@ echo  "upstream user2 {" >> default.conf
 docker ps|grep -v CONTAINER|grep user2|awk '{print $1}'|xargs docker inspect --format '{{ .NetworkSettings.IPAddress }}'|awk '{print "server " $1 ":80;"}' >> default.conf
 echo  "}" >> default.conf
 
-cat ../conf/nginx2.conf >> ../default.conf
+cat ../conf/nginx2.conf >> default.conf
 cd ..
 docker build -t jkepeer1/loadbalanser .
 docker run -d -p 80:80 jkepeer1/loadbalanser
